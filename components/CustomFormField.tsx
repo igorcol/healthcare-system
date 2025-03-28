d08bb52e46cf41dee23c43f46a066d1120ac0fad
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-27 21:18:01 -0300

    feat: Initial Patient Form customization

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+import React, { ReactNode } from "react";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+
+interface CustomProps {
+    control: Control<any>
+    fieldType: FormFieldType
+    name: string
+    label?: string
+    placeholder?: string
+    iconSrc?: string
+    iconAlt?: string
+    disabled?: boolean
+    fateFormat?: string
+    showTimeSelect?: boolean
+    children?: ReactNode
+    renderSkeleton?: (field: any) => ReactNode
+}
+
+const CustomFormField = ({control, fieldType, name, label}: CustomProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+            {fieldType !== FormFieldType.CHECKBOX && label && (
+                <FormLabel>{label}</FormLabel>
+            )}
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
